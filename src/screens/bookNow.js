2341303c84adaa987456356fd6a1f639f3c21e3d
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-12-15 00:13:01 +0400

    book now screen finished

--- src/screens/bookNow.js
+++ src/screens/bookNow.js
@@ -5,18 +5,16 @@ import {
   RefreshControl,
   ScrollView,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { Calendar, LocaleConfig } from "react-native-calendars";
-import { dataCategories } from "../data/dataArrays";
 
 export default function BookNowScreen(props) {
   const [selected, setSelected] = useState("");
   const [refreshing, setRefreshing] = useState(false);
   const { navigation, route } = props;
   const item = route.params;
-
-  console.log(item);
 
   useEffect(() => {
     navigation.setOptions({
@@ -91,17 +89,126 @@ export default function BookNowScreen(props) {
     }, 2000);
   };
 
+  // ACTIVE CLASS ADD
+  const [selectedHours, setSelectedHours] = useState(1);
+
+  const activeClassAdd = (date) => {
+    setSelectedHours(date);
+  };
+
   const renderOpenHours = ({ item }) => {
     return (
-      <View>
-        <Text>{item}</Text>
-      </View>
+      <TouchableOpacity
+        style={
+          item === selectedHours
+            ? {
+                padding: 10,
+                paddingHorizontal: 20,
+                borderWidth: 1,
+                borderStyle: "solid",
+                borderColor: "#FB9400",
+                marginHorizontal: 10,
+                borderRadius: 20,
+                backgroundColor: "#FB9400",
+              }
+            : {
+                padding: 10,
+                paddingHorizontal: 20,
+                borderWidth: 1,
+                borderStyle: "solid",
+                borderColor: "#FB9400",
+                marginHorizontal: 10,
+                borderRadius: 20,
+              }
+        }
+        onPress={() => {
+          activeClassAdd(item);
+        }}
+      >
+        <Text
+          style={
+            item === selectedHours ? { color: "white" } : { color: "#FB9400" }
+          }
+        >
+          {item}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  // RENDER SPECIALIST
+  const [selectedSpecialist, setSelectedSpecialist] = useState(1);
+
+  const renderSpecialist = ({ item }) => {
+    return (
+      <TouchableOpacity
+        style={
+          item?.name === selectedSpecialist
+            ? {
+                padding: 15,
+                borderWidth: 1,
+                borderStyle: "solid",
+                borderColor: "#FB9400",
+                marginHorizontal: 10,
+                borderRadius: 20,
+                marginBottom: 40,
+              }
+            : {
+                padding: 15,
+                marginHorizontal: 10,
+                borderRadius: 20,
+              }
+        }
+        onPress={() => {
+          setSelectedSpecialist(item?.name);
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={{ uri: item.profilePhoto }}
+            style={{ width: 100, height: 100, borderRadius: 15 }}
+          />
+        </View>
+
+        <Text
+          style={
+            item?.name === selectedSpecialist
+              ? {
+                  color: "#FB9400",
+                  fontWeight: 700,
+                  fontSize: 16,
+                  textAlign: "center",
+                  paddingVertical: 8,
+                }
+              : {
+                  color: "black",
+                  fontWeight: 700,
+                  fontSize: 16,
+                  textAlign: "center",
+                  paddingVertical: 8,
+                }
+          }
+        >
+          {item.name}
+        </Text>
+
+        <Text
+          style={{
+            color: "grey",
+            fontWeight: 700,
+            fontSize: 16,
+            textAlign: "center",
+          }}
+        >
+          {item.job}
+        </Text>
+      </TouchableOpacity>
     );
   };
 
   return (
     <ScrollView
-      style={{ paddingVertical: 10, paddingHorizontal: 10 }}
+      style={{ marginVertical: 10, paddingHorizontal: 10 }}
       refreshControl={
         <RefreshControl
           refreshing={refreshing}
@@ -114,6 +221,7 @@ export default function BookNowScreen(props) {
       <Text style={{ fontWeight: 700, fontSize: 20, paddingVertical: 20 }}>
         Tarix Seçin
       </Text>
+
       <Calendar
         onDayPress={(day) => {
           setSelected(day.dateString);
@@ -138,6 +246,32 @@ export default function BookNowScreen(props) {
         renderItem={renderOpenHours}
         keyExtractor={(item, index) => index}
       />
+
+      <Text style={{ fontWeight: 700, fontSize: 20, paddingVertical: 20 }}>
+        Mütəxəssis Seçin
+      </Text>
+
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={item?.specialist}
+        renderItem={renderSpecialist}
+        keyExtractor={(item, index) => index}
+      />
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#FB9400",
+          paddingVertical: 18,
+          borderRadius: 50,
+          marginBottom: 15,
+        }}
+        onPress={() => {}}
+      >
+        <Text style={{ textAlign: "center", fontWeight: 700, fontSize: 18 }}>
+          Rezerv Et
+        </Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 }
