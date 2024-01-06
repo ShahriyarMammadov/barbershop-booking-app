2ebcd03b565ea93d791c194ce02729d9743f0f76
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2024-01-06 22:01:32 +0400

    maps advanced modal added

--- src/screens/explore.js
+++ src/screens/explore.js
@@ -1,34 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Platform,
   Linking,
   Text,
   View,
   Alert,
-  RefreshControl,
+  Image,
+  TouchableOpacity,
+  Dimensions,
 } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
+import LinearGradient from "react-native-linear-gradient";
+import { Modalize } from "react-native-modalize";
 
 export default function ExploreScreen(props) {
   const navigation = props;
+
+  const width = Dimensions.get("window").width;
+  const height = Dimensions.get("window").height;
+
   const [location, setLocation] = useState({
     coords: {
       latitude: 40.4144301,
       longitude: 50.11,
     },
   });
-  // const [refreshing, setRefreshing] = useState(false);
 
   const locations = [
     {
       id: 1,
-      title: "Yer 1",
+      title: "Şəhriyar BarberShop",
       coordinates: { latitude: 40.4144301, longitude: 50.11 },
     },
     {
       id: 2,
-      title: "Yer 2",
+      title: "Ramil BeautySaloon",
       coordinates: { latitude: 40.4144301, longitude: 50.117 },
     },
   ];
@@ -76,25 +83,39 @@ export default function ExploreScreen(props) {
 
   console.log(location);
 
-  // const onRefresh = () => {
-  //   setRefreshing(true);
-  //   getLocation();
-  //   setRefreshing(false);
-  // };
+  const [isModalVisible, setModalVisible] = React.useState(false);
+  const modalizeRef = useRef(null);
+
+  const onOpen = () => {
+    setModalVisible(true);
+  };
+
+  const onClose = () => {
+    setModalVisible(false);
+  };
+
+  const getData = () => [
+    { id: "1", heading: "Item 1" },
+    { id: "2", heading: "Item 2" },
+    { id: "3", heading: "Item 3" },
+  ];
+
+  const onMapPress = () => {
+    modalizeRef.current?.open();
+  };
+
+  const renderItem = ({ item }) => (
+    <View
+      style={{ padding: 16, borderBottomWidth: 1, borderBottomColor: "#ccc" }}
+    >
+      <Text>{item.heading}</Text>
+    </View>
+  );
 
   return (
-    <View
-    // refreshControl={
-    //   <RefreshControl
-    //     refreshing={refreshing}
-    //     onRefresh={onRefresh}
-    //     tintColor="#3F51B5"
-    //     title="Refreshing..."
-    //   />
-    // }
-    >
-      <View>
-        {location ? (
+    <View>
+      {locations ? (
+        <>
           <MapView
             style={{ width: "100%", height: "100%" }}
             initialRegion={{
@@ -103,27 +124,58 @@ export default function ExploreScreen(props) {
               latitudeDelta: 0.01,
               longitudeDelta: 0.01,
             }}
-            mapType="hybrid"
+            mapType="satellite"
             showsUserLocation={true}
           >
             {locations.map((location) => (
               <Marker
-                key={location.id}
-                coordinate={location.coordinates}
-                title={location.title}
-              />
+                key={location?.id}
+                coordinate={location?.coordinates}
+                title={location?.title}
+                // onPress={() => handleMarkerPress(location)}
+                onPress={onMapPress}
+              >
+                <View style={{ padding: 5, alignItems: "center" }}>
+                  <Image
+                    source={{
+                      uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxY01pBUoDkZSG250Kvs6OvUfAnnv3m5lDlw&usqp=CAU",
+                    }}
+                    style={{ width: 40, height: 40, borderRadius: 50 }}
+                  />
+                  <Text
+                    style={{
+                      fontWeight: 700,
+                      textAlign: "center",
+                      backgroundColor: "grey",
+                      color: "white",
+                      padding: 5,
+                      paddingHorizontal: 10,
+                      fontSize: 12,
+                    }}
+                  >
+                    {location?.title}
+                  </Text>
+                </View>
+              </Marker>
             ))}
           </MapView>
-        ) : (
-          <Text
-            style={{
-              textAlign: "center",
+
+          <Modalize
+            ref={modalizeRef}
+            // modalHeight={height / 2}
+            snapPoint={height / 2} // Snapping'in gerçekleşeceği yükseklik
+            modalHeight={height - 200}
+            flatListProps={{
+              data: getData(),
+              renderItem: renderItem,
+              keyExtractor: (item) => item.heading,
+              showsVerticalScrollIndicator: false,
             }}
-          >
-            Loading...
-          </Text>
-        )}
-      </View>
+          />
+        </>
+      ) : (
+        <Text style={{ textAlign: "center" }}>Xəritə Yüklənir...</Text>
+      )}
     </View>
   );
 }
